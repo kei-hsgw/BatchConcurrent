18b1eae08f15f122d1bd792fdb7dae1e94afca35
keisukehasegawa0918 <chiro9689@gmail.com>
2022-11-22 09:25:06 +0900

    SecondTasklet実装

--- src/main/java/com/example/demo/tasklet/SecondTasklet.java
+++ src/main/java/com/example/demo/tasklet/SecondTasklet.java
@@ -1,5 +1,25 @@
 package com.example.demo.tasklet;
 
-public class SecondTasklet {
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@StepScope
+@Slf4j
+public class SecondTasklet implements Tasklet {
+	
+	@Override
+	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+		
+		log.info("SecondTasklet");
+		
+		return RepeatStatus.FINISHED;
+	}
 
 }
